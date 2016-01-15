23e1ad3ab6730311a72e3d7ea0870b12ba7c2d9c
angelslanding <angelsown4056@yahoo.com>
2016-01-15 00:31:16 +0000

    add user stock association and display

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,4 +1,8 @@
-class Stock < ActiveRecord::Base 
+class Stock < ActiveRecord::Base
+    
+    belongs_to :user
+    belongs_to :stock
+    
     def self.find_by_ticker(ticker_symbol)
         where(ticker: ticker_symbol).first 
     end 
